b5377d6ad7ae1b78656510ba0489cc1e4baeb9f6
riya jain <rjain35@illinois.edu>
2019-02-04 00:29:33 -0600

    added siebelForTesting.json and DataForTesting class

--- test/com/Adventures/AdventuresTest.java
+++ test/com/Adventures/AdventuresTest.java
@@ -1,8 +1,13 @@
 package com.Adventures;
 
+import com.google.gson.Gson;
+import org.junit.Before;
 import org.junit.Test;
 
 public class AdventuresTest {
 
-
+    @Before
+    public void setUp() throws Exception {
+        Gson gson = new Gson();
+    }
 }
